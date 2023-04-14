36c8f5fb82d03a950c9b7ee33e613db2ff305bbf
MrAgui <karlaguilar3800@gmail.com>
2023-04-14 21:11:23 +0800

    add campusAPI

--- routes/campus/campusAPI.js
+++ routes/campus/campusAPI.js
@@ -1,3 +1,14 @@
+const addCampusRoute = require('./addCampus');
+const deleteCampusRoute = require('./deleteCampus');
+const addDepartmentRoute = require('./addDepartment');
+const deleteDepartmentRoute = require('./deleteDepartment');
+
 const campusAPI = require('express').Router();
+
+campusAPI.use('./add', addCampusRoute)
+campusAPI.use('./delete', deleteCampusRoute)
+
+campusAPI.use('./add', addDepartmentRoute)
+campusAPI.use('./delete', deleteDepartmentRoute)
 
 module.exports = campusAPI;
