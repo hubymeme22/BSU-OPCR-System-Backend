6c1420f8e2f032be0d7380be68664a284b46118c
MrAgui <karlaguilar3800@gmail.com>
2023-04-14 21:11:50 +0800

    Still not working, confused how to implement.

--- routes/campus/addCampus.js
+++ routes/campus/addCampus.js
@@ -1,1 +1,20 @@
 // POST /api/campus/:campusName
+const routeOp = require('../../modules/campusOperations');
+const params = require('../../modules/paramCheck');
+const addCampusRoute = require('express').Router();
+
+addCampusRoute.post('/',(req, res) =>{
+    let missedParams = params.paramCheck(['campus_name', 'department_name', 'assigned_to' ,'opcr'], req.body);
+    const responseFormat={added: false, summary:{}, error: null};
+
+    if(missedParams.length > 0) {
+        responseFormat.error = `MissedParams=${missedParams}`;
+        return res.json(responseFormat);
+    }
+
+    //still clueless what to do next here.
+
+    routeOp.addCampus(req.body.campus_name, req.body.department_name, req.body.assigned_to, req.body.opcr);
+})
+
+module.exports = addCampusRoute;
