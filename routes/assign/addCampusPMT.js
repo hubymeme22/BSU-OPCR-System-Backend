6391137010dc0b9d563a9922de3bec0aacf0fea1
HueHueberry <huby.new1112@gmail.com>
2023-04-17 01:31:33 +0800

    campus api route fix

--- routes/assign/addCampusPMT.js
+++ routes/assign/addCampusPMT.js
@@ -6,7 +6,7 @@ const assignCampusPmt = require('express').Router();
 const params = require('../../modules/paramCheck');
 const routeOp = require('../../modules/assignOperations');
 
-assignCampusPmt.post('/campus/:campusName', (req, res) => {
+assignCampusPmt.post('/:campusName', (req, res) => {
     const missedParams = params.paramCheck(['scope'], req.body);
     if (missedParams.length > 0)
         return res.json({ assigned: false, error: `MissedParams=${missedParams}` });
