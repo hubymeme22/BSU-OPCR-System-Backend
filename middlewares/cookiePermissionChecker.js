713358ff7a802e56d058b901eaf3eb58498729d1
HueHueberry <huby.new1112@gmail.com>
2023-04-19 17:26:44 +0800

    Additional permission check for cookies

--- middlewares/cookiePermissionChecker.js
+++ middlewares/cookiePermissionChecker.js
@@ -16,4 +16,39 @@ module.exports.permissionCheck = (req, res, next) => {
         req.allowedDataErr = true;
         next();
     }
-}
+};
+
+module.exports.specifiedPermissionCheck = (permission=[]) => {
+    const permissionCheck = (req, res, next) => {
+        const cookies = req.cookies;
+        const missedParams = param.paramCheck(['token'], cookies);
+    
+        // req.allowedData not assigned
+        if (missedParams.length > 0) next();
+        try {
+            const derivedInfo = jwt.verify(cookies.token, process.env.SECRETKEY);
+            const userdata = derivedInfo.userdata;
+
+            // checks if the permission of the user is allowed
+            let allowed = true;
+            permission.forEach(perm => {
+                if (userdata.access.indexOf(perm) < 0)
+                    return allowed = false;
+            });
+
+            if (!allowed) {
+                req.allowedDataErr = false;
+                next();
+            }
+
+            req.allowedData = derivedInfo;
+            req.allowedDataErr = false;
+            next();
+        } catch(err) {
+            req.allowedDataErr = true;
+            next();
+        }
+    };
+
+    return permissionCheck;
+};
