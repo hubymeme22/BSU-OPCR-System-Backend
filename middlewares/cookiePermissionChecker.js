127be50d319349c750da76c9105ed3021b3552ed
HueHueberry <huby.new1112@gmail.com>
2023-04-19 23:07:34 +0800

    Added functions for validating account access

--- middlewares/cookiePermissionChecker.js
+++ middlewares/cookiePermissionChecker.js
@@ -6,7 +6,11 @@ module.exports.permissionCheck = (req, res, next) => {
     const missedParams = param.paramCheck(['token'], cookies);
 
     // req.allowedData not assigned
-    if (missedParams.length > 0) next();
+    if (missedParams.length > 0) {
+        req.allowedDataErr = true;
+        return next();
+    }
+
     try {
         const derivedInfo = jwt.verify(cookies.token, process.env.SECRETKEY);
         req.allowedData = derivedInfo;
@@ -18,27 +22,38 @@ module.exports.permissionCheck = (req, res, next) => {
     }
 };
 
+// specifies all the permission the user must have
 module.exports.specifiedPermissionCheck = (permission=[]) => {
     const permissionCheck = (req, res, next) => {
         const cookies = req.cookies;
         const missedParams = param.paramCheck(['token'], cookies);
-    
+
         // req.allowedData not assigned
-        if (missedParams.length > 0) next();
+        if (missedParams.length > 0) {
+            req.allowedDataErr = true;
+            return next();
+        }
+
         try {
             const derivedInfo = jwt.verify(cookies.token, process.env.SECRETKEY);
             const userdata = derivedInfo.userdata;
 
-            // checks if the permission of the user is allowed
+            // additional checking to avoid bruteforing token for empty permission
             let allowed = true;
-            permission.forEach(perm => {
-                if (userdata.access.indexOf(perm) < 0)
-                    return allowed = false;
-            });
+            if (permission.length <= 0) {
+                allowed = false;
+            } else {
+                // checks if the permission of the user is allowed
+                permission.forEach(perm => {
+                    if (userdata.access.indexOf(perm) < 0) {
+                        return allowed = false;
+                    }
+                });
+            }
 
             if (!allowed) {
-                req.allowedDataErr = false;
-                next();
+                req.allowedDataErr = true;
+                return next();
             }
 
             req.allowedData = derivedInfo;
@@ -52,3 +67,54 @@ module.exports.specifiedPermissionCheck = (permission=[]) => {
 
     return permissionCheck;
 };
+
+// specifies all the permission the user can have
+module.exports.allowPermission = (permission=[]) => {
+    const permissionCheck = (req, res, next) => {
+        const cookies = req.cookies;
+        const missedParams = param.paramCheck(['token'], cookies);
+    
+        // req.allowedData not assigned
+        if (missedParams.length > 0) {
+            req.allowedDataErr = true;
+            return next();
+        }
+
+        try {
+            const derivedInfo = jwt.verify(cookies.token, process.env.SECRETKEY);
+            const userdata = derivedInfo.userdata;
+
+            let allowed = false;
+            permission.forEach(perm => {
+                if (userdata.access.indexOf(perm) < 0)
+                    return allowed = true;
+            });
+
+            if (!allowed) {
+                req.allowedDataErr = true;
+                return next();
+            }
+
+            req.allowedData = derivedInfo;
+            req.allowedDataErr = false;
+            next();
+        } catch(err) {
+            req.allowedDataErr = true;
+            next();
+        }
+    };
+
+    return permissionCheck;
+};
+
+// a simple error generation function that will be called
+// when cookies are being checked
+module.exports.permissionErrGen = (jsonFormat) => {
+    jsonFormat.error = 'InsufficientPermission';
+    const middleware = (req, res, next) => {
+        if (req.allowedDataErr)
+            return res.json(jsonFormat);
+        next();
+    }
+    return middleware;
+}
