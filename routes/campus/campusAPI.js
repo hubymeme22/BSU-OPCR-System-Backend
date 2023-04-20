ff6af59e00c8b858124dcce3b6dcd2f8246af273
MrAgui <karlaguilar3800@gmail.com>
2023-04-21 04:39:28 +0800

    modified campusAPI, fixed some semicolon

--- routes/campus/campusAPI.js
+++ routes/campus/campusAPI.js
@@ -1,20 +1,21 @@
-const addCampusRoute = require('./addCampus');
 const deleteCampusRoute = require('./deleteCampus');
 const addDepartmentRoute = require('./addDepartment');
-const deleteDepartmentRoute = require('./deleteDepartment');
+// const deleteDepartmentRoute = require('./deleteDepartment');
 const getAllCampusRoute = require('./readCampus');
 const addCampusWithDepartmentRoute = require('./addCampusWithDepartment');
+const editDepartmentRoute = require('./editDepartment');
 
 const campusAPI = require('express').Router();
 
 campusAPI.use('/', getAllCampusRoute);
 
 
-campusAPI.use('/addCampusWithDepartment', addCampusWithDepartmentRoute)
-
+campusAPI.use('/addCampusWithDepartment', addCampusWithDepartmentRoute);
 campusAPI.use('/deleteCampus', deleteCampusRoute);
+
 
 // campusAPI.use('/add', addDepartmentRoute);
 // campusAPI.use('/delete', deleteDepartmentRoute);
+// campusAPI.use('/editDepartment', editDepartmentRoute);
 
 module.exports = campusAPI;
